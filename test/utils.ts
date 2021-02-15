1231ca7afd2568509e52b5a2ba55233e23c6df5b
Shishir <sramam@users.noreply.github.com>
2021-02-15 10:12:40 -0800

    Revert "chore: control update of schema.json via env variable"
    This reverts commit 113fdfb709367269445df6efd6ca527e65767a10.

--- test/utils.ts
+++ test/utils.ts
@@ -47,14 +47,11 @@ export function assertValidSchema(
         const actual: any = JSON.parse(JSON.stringify(schema));
 
         // uncomment to write test files
-        if (process.env.UPDATE_SCHEMA) {
-            console.log(`updating schema ${basePath}/${relativePath}/schema.json`);
-            writeFileSync(
-                resolve(`${basePath}/${relativePath}/schema.json`),
-                JSON.stringify(schema, null, 4) + "\n",
-                "utf8"
-            );
-        }
+        writeFileSync(
+            resolve(`${basePath}/${relativePath}/schema.json`),
+            JSON.stringify(schema, null, 4) + "\n",
+            "utf8"
+        );
 
         expect(typeof actual).toBe("object");
         expect(actual).toEqual(expected);
